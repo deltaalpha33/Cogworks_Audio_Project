dde6181a75c57462e961b9969f39e265f0ffbf28
thedashdude <thedashdude@yahoo.com>
2017-07-14 10:47:05 -0400

    added giving song data from the filename

--- song/song.py
+++ song/song.py
@@ -1,7 +1,28 @@
 class Song:
-    def __init__(self,author='Unknown',title='SongName',requencyDeltas):
+    def __init__(self):
+        self.author = 'unknown'
+        self.title = 'unknown'
+        self.frequencyDeltas = []
+    def __init__(self,frequencyDeltas, author,title):
         self.author = author
         self.title = title
         self.frequencyDeltas = frequencyDeltas
+
     def get_name(self):
         return self.author + " - " + self.title
+    def author_title_from_filename(self,filename):
+        """sets title and author from a filename
+
+        Parameters
+        ----------
+        filename: the string name of the file. (not the path)
+
+        Returns
+        -------
+        Nothing
+        """
+        filename = filename.replace('.mp3','')
+        filename = filename.replace('_',' ')
+        parts = filename.split(' - ')
+        self.author = parts[0]
+        self.title = parts[1]
