ff51fa6e1115d983ca55cea385314a19021cdcf7
deltaalpha33 <dalexakos@gmail.com>
2017-07-14 16:03:57 -0400

    fixed previous commit

--- fourier/discrete_transform.py
+++ fourier/discrete_transform.py
@@ -8,7 +8,8 @@ from scipy.ndimage.filters import maximum_filter
 from scipy.ndimage.morphology import generate_binary_structure, binary_erosion 
 from scipy.ndimage.morphology import iterate_structure
 
-def filterlowamplitudes(C, threshold):
+class Fourier(class):
+    def filterlowamplitudes(C, threshold):
     """ Read
 
         Parameters
@@ -26,70 +27,65 @@ def filterlowamplitudes(C, threshold):
     m[ge] = True
     return m
 
-def local_peaks(data):
-    """ Find local peaks in a 2D array of data.
+    def local_peaks(data):
+        """ Find local peaks in a 2D array of data.
 
-    Parameters
-    ----------
-    data : numpy.ndarray
-    
-    Returns
-    -------
-    Binary indicator, of the same shape as `data`. The value of
-    True indicates a local peak. """
-	ffp = generate_binary_structure(rank=2 , connectivity=2)
-    ##acceptable_values = data > 1
-    peaks = data == (maximum_filter(data, footprint=fp))
-    ##acceptable_peaks = np.logical_and(peaks, acceptable_values)
-    ##return acceptable_peaks
-    return peaks
+        Parameters
+        ----------
+        data : numpy.ndarray
+        
+        Returns
+        -------
+        Binary indicator, of the same shape as `data`. The value of
+        True indicates a local peak. """
+        ffp = generate_binary_structure(rank=2 , connectivity=2)
+        ##acceptable_values = data > 1
+        peaks = data == (maximum_filter(data, footprint=fp))
+        ##acceptable_peaks = np.logical_and(peaks, acceptable_values)
+        ##return acceptable_peaks
+        return peaks
 
-def pair_frequencies(data, peaks , look_ahead = 15):
-    """ Calculate Differences between  to sets of peaks represented  in 2D arrays of data.
+    def pair_frequencies(data, peaks , look_ahead = 15):
+        """ Calculate Differences between  to sets of peaks represented  in 2D arrays of data.
 
-    Parameters
-    ----------
-    data : numpy.ndarray
-    peaks : numpy.ndarray 
-        mask of peaks
-    look_ahead : int
-        how many other points of data to consider
+        Parameters
+        ----------
+        data : numpy.ndarray
+        peaks : numpy.ndarray 
+            mask of peaks
+        look_ahead : int
+            how many other points of data to consider
 
-    Returns
-    -------
-    Binary indicator, of the same shape as `data`. The value of
-    True indicates a local peak. """
+        Returns
+        -------
+        Binary indicator, of the same shape as `data`. The value of
+        True indicates a local peak. """
 
-    masked_data = ma.masked_array(data,  mask=peaks)
+        masked_data = ma.masked_array(data,  mask=peaks)
 
-    time_array = np.hsplit(x, x.shape[0]) #list of numpy arrays
-    
-    analyzed_freq = list()
-    for current_time in range(len(time_array) - look_ahead): #dont compare last element(s)
-        current_frequencies = time_array[current_time]
-        for frequency1 in current_frequencies:
-            
-            for compare_time in range(look_ahead):
-                compare_frequencies = time_array[compare_time]
-                for frequency2 in compare_frequencies:
-                    delta_t = compare_time - current_time
-                    analyzed_freq.append((frequency1, frequency2, delta_t))
-    return analyzed_freq
+        time_array = np.hsplit(x, x.shape[0]) #list of numpy arrays
+        
+        analyzed_freq = list()
+        for current_time in range(len(time_array) - look_ahead): #dont compare last element(s)
+            current_frequencies = time_array[current_time]
+            for frequency1 in current_frequencies:
+                
+                for compare_time in range(look_ahead):
+                    compare_frequencies = time_array[compare_time]
+                    for frequency2 in compare_frequencies:
+                        delta_t = compare_time - current_time
+                        analyzed_freq.append((frequency1, frequency2, delta_t))
+        return analyzed_freq
 
 
-
-class Song:
-    def __init__(self, audio_data):
-        self.author = 'unknown'
-        self.title = 'unknown'
-        self.frequencyDeltas = []
+class Song(Fourier):
     def __init__(self,frequencyDeltas, author,title):
         self.author = author
         self.title = title
-        self.frequencyDeltas = frequencyDeltas
 
     def get_name(self):
         return self.author + " - " + self.title
+
     def author_title_from_filename(self,filename):
         """sets title and author from a filename
 
@@ -107,21 +103,16 @@ class Song:
         self.author = parts[0]
         self.title = parts[1]
 
-    def isSubset(origin_data, compare_data):
-    """ Find if the two pieces of data are correlated
+    def isSimilar(self, compare_song):
+    """ Find if the compare_song is a match to the current song
 
     Parameters
     ----------
-    origin_data : Song
-    peaks : list
-        mask of peaks
-    look_ahead : int
-        how many other points of data to consider
+    compare_song : Song
 
     Returns
     -------
-    Binary indicator, of the same shape as `data`. The value of
-    True indicates a local peak. """
+    The similarity of the songs as an integer"""
             
             
 
